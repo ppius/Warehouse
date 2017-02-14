353f5bda2382d6ed96b630ec54f332a7316c40e3
Peace <peacepius716@gmail.com>
2017-02-14 12:06:04 -0500

    First commit

---
+++ footwear.py
@@ -0,0 +1,85 @@
+class Footwear:
+    """
+      >>> f = Footwear('Sling-back', 8.5, '1234-0')
+      >>> f.style
+      'Sling-back'
+      >>> f.size
+      8.5
+      >>> f.sku
+      '1234-0'
+      >>> f.type
+      'Unspecified'
+      >>> f.print_size()
+      'size 8½'
+      >>> f2 = Footwear('Hightop', 10, '1234-19')
+      >>> f2.print_size()
+      'size 10'
+      >>> print(f)
+      Sling-back (size 8½)
+      >>> print(f2)
+      Hightop (size 10)
+    """
+    def __init__(self, style, size, sku, shoetype='Unspecified'):
+        self.style = style
+        self.size = size
+        self.sku = sku
+        self.type = shoetype
+
+    def print_size(self):
+        if int(self.size) == self.size:
+            return 'size {0}'.format(int(self.size))
+        return 'size {0}½'.format(int(self.size))
+
+    def __str__(self):
+        return '{0} ({1})'.format(self.style, self.print_size())
+
+
+class Boot(Footwear):
+    """
+      >>> b = Boot('Hiking', 11.5, '1234-10')
+      >>> print(b)
+      Boot - Hiking (size 11½)
+    """
+    def __init__(self, style, size, sku, shoetype='Boot'):
+        Footwear.__init__(self, style, size, sku, shoetype)
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.type, Footwear.__str__(self))
+
+
+class Shoe(Footwear):
+    """
+      >>> s = Shoe('Generic', 9.5, '1234-23')
+      >>> print(s)
+      Shoe - Generic (size 9½)
+    """
+    def __init__(self, style, size, sku, shoetype='Shoe'):
+        Footwear.__init__(self, style, size, sku, shoetype)
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.type, Footwear.__str__(self))
+
+
+class DressShoe(Shoe):
+    """
+      >>> ds = DressShoe('Sling-back', 8.5, '1234-43')
+      >>> print(ds)
+      Dress Shoe - Sling-back (size 8½)
+    """
+    def __init__(self, style, size, sku, shoetype='Dress Shoe'):
+        Shoe.__init__(self, style, size, sku, shoetype)
+
+
+class CasualShoe(Shoe):
+    """
+      >>> cs = CasualShoe('Moccasin', 12.5, '1234-62')
+      >>> print(cs)
+      Casual Shoe - Moccasin (size 12½)
+    """
+    def __init__(self, style, size, sku, shoetype='Casual Shoe'):
+        Shoe.__init__(self, style, size, sku, shoetype)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ warehouse.py
@@ -0,0 +1,34 @@
+class Bin:
+    """
+    bin class holds bin items
+    """
+    def __init__(self, a_bin):
+        self.name = a_bin
+        self.contents = []
+        
+    def add(self, item):
+        for con in self.contents:
+            if item.sku < con.sku:
+                return
+        self.contents.append(item)
+    
+    def __str__(self):
+        s = "Bin {0}:".format(self.name)
+        for item in self.contents:
+            s += "\n  {0}".format(item)
+        return s
+
+class BinItem:
+    """
+    this is an item from the warehouse
+    """
+    def __init__(self, sku, quantity):
+        self.sku = sku
+        self.quantity = quantity
+    
+    def __str__(self):
+        return "SKU {0}: {1}".format(self.sku, self.quantity)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("tests03.txt")
