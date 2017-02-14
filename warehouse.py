c35be26b91399cbf1404f0df21444f1eb6a1a41e
ppius <peacepius716@gmail.com>
2017-02-14 12:20:24 -0500

    Update warehouse.py

--- warehouse.py
+++ warehouse.py
@@ -1,34 +1,39 @@
-class Bin:
-    """
-    bin class holds bin items
-    """
-    def __init__(self, a_bin):
-        self.name = a_bin
-        self.contents = []
-        
-    def add(self, item):
-        for con in self.contents:
-            if item.sku < con.sku:
-                return
-        self.contents.append(item)
-    
-    def __str__(self):
-        s = "Bin {0}:".format(self.name)
-        for item in self.contents:
-            s += "\n  {0}".format(item)
-        return s
-
 class BinItem:
-    """
-    this is an item from the warehouse
-    """
+    """A warehouse bin item consisting of a sku number and quantity."""
     def __init__(self, sku, quantity):
         self.sku = sku
         self.quantity = quantity
-    
+
     def __str__(self):
         return "SKU {0}: {1}".format(self.sku, self.quantity)
 
+
+class Bin:
+    """A location for storing BinItems."""
+    def __init__(self, name):
+        self.name = name
+        self.contents = []
+
+    def __str__(self):
+        s = "Bin {0}:".format(self.name)
+        for item in self.contents:
+            s += '\n  ' + item.__str__()
+        return s
+
+    def add(self, item):
+        for index, value in enumerate(self.contents):
+            if value.sku == item.sku:
+                value.quantity += item.quantity
+                break
+            if value.sku < item.sku:
+                continue
+            else:
+                self.contents.insert(index, item)
+                break
+        else:
+            self.contents.append(item)
+
+
 if __name__ == '__main__':
     import doctest
-    doctest.testfile("tests03.txt")
+    doctest.testfile("bin_tests.txt")
